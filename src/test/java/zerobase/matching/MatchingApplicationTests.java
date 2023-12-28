eeca36b2c858564d3b165a101fcd5c840e97f6ff
DdangKkong <dnjsdn8@naver.com>
2023-12-28 16:23:17 +0900

    first commit

---
+++ src/test/java/zerobase/matching/MatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package zerobase.matching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
